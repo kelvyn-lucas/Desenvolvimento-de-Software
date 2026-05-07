a409f6526156c9b24770a5a0fce238abbf935c2a
kelvyn-lucas <146493217+kelvyn-lucas@users.noreply.github.com>
2026-05-06 21:38:23 -0300

    Add files via upload

---
+++ aula 06-05-2026/Formas2D/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       // Formas2D 2d = new Formas2D(6.5, 5.0, "retangulo");
+       Triangulo isosceles = new Triangulo(5.0, "Triangulo", "isósceles");
+       
+    }
+}
